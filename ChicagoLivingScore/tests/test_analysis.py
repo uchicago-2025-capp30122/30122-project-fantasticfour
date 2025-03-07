d755db91c53d6785c0e9b63f2d8d1334ab9bcdea
Hamzatariquchicago <hamzatariq@uchicago.edu>
2025-03-07 17:15:05 -0600

    test_analysis.py

--- ChicagoLivingScore/tests/test_analysis.py
+++ ChicagoLivingScore/tests/test_analysis.py
@@ -2,11 +2,13 @@ import pytest
 from pathlib import Path
 from analysis.economic_infrastructure_analysis import main
 
-data = Path(__file__).parent.parent / "data/raw_data/raw_data_eco_infra.csv"
-df_eco = main(data)
+@pytest.fixture
+def df_eco():
+    data = Path(__file__).parent.parent / "data/raw_data/raw_data_eco_infra.csv"
+    return main(data)
 
 def test_normalize_max(df_eco):
     assert df_eco["unemployed"].max() <= 1, "Test failed: normalized value greater than 1 found!"
-    
-def test_chicago_zip():
+
+def test_chicago_zip(df_eco):
     assert len(df_eco["zipcode"]) <= 60, "Test failed: you have more than actual zipcodes in Chicago"
