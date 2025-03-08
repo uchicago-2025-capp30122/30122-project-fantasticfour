5d73ae13c522d68dc0989b00bbcc5ff6821b7d17
ChangYuri <106869335+ChangYuri@users.noreply.github.com>
2025-03-07 19:16:16 -0600

    hamza test

--- ChicagoLivingScore/tests/test_analysis.py
+++ ChicagoLivingScore/tests/test_analysis.py
@@ -5,12 +5,12 @@ from analysis.economic_infrastructure_analysis import main
 
 @pytest.fixture
 def df_eco():
-    data_path = Path("ChicagoLivingScore/data/cleaned_data/cleaned_data_economic_infrastructure.csv")
-    
+    BASE_DIR = Path(__file__).parent.parent
+    DATA_FILE = BASE_DIR / "data" / "cleaned_data" /  "cleaned_data_economic_infrastructure.csv"
     # Ensure the file exists before reading
-    assert data_path.exists(), f"test failed: data not found!"
+    assert DATA_FILE.exists(), f"test failed: data not found!"
     
-    return pd.read_csv(data_path)
+    return pd.read_csv(DATA_FILE)
 
 def test_normalize_max(df_eco):
     assert df_eco["unemployed"].max() <= 1, "test failed: normalized value greater than 1 found!"
