e3a191d96ff40b69ed57f9fa85b0f7d666708a2b
Hamzatariquchicago <hamzatariq@uchicago.edu>
2025-03-09 16:47:14 -0500

    test_analysis.py

--- ChicagoLivingScore/tests/test_analysis.py
+++ ChicagoLivingScore/tests/test_analysis.py
@@ -7,11 +7,11 @@ from pathlib import Path
 def df_eco():
     file_path = Path("data/cleaned_data/cleaned_data_economic_infrastructure.csv")
     # chek if data loads correctly
-    assert file_path.exists(), f"test failed: data not found!"
+    assert file_path.exists(), f"test failed: data not found"
     return pd.read_csv(file_path)
 
 def test_normalize_max(df_eco):
-    assert df_eco["unemployed"].max() <= 1, "test failed: normalized value greater than 1 found!"
+    assert df_eco["unemployed"].max() <= 1, "test failed: normalized value can't be greater than 1"
 
 def test_chicago_zip(df_eco):
-    assert len(df_eco["zipcode"].unique()) <= 60, "test failed: you have more zipcodes than actual ones in Chicago!"
+    assert len(df_eco["zipcode"].unique()) <= 60, "test failed: more zipcodes than actual ones in Chicago"
